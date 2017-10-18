e33eab281109e67fd4e7c0c8a20b3a96874aab4a
Remko Silvis <remko@secutor.nl>
2017-10-18 21:38:48 +0200

    Hex seems to be working

--- src/Set1/Hex.php
+++ src/Set1/Hex.php
@@ -7,104 +7,32 @@ use InvalidArgumentException;
 /**
  * Encode and decode strings to hexadecimal encoding.
  */
-
-class Hex {
+class Hex
+{
 
     /**
      * List of possible chars in the encoding.
      *
      * @var string $charlist
      */
-    private static $charlist = '0123456789ABCDEF';
+    private static $charlist = '0123456789abcdef';
 
-    /**
-     * Convert a decimal number to a hexadecimal character.
-     *
-     * @param int $num the number to convert to a character.
-     * @return string
-     * @throws InvalidArgumentException if the decimal number is 0 > $num > 15.
-     */
-    private static function toChar(int $num) :string
+    private function binToDec(int $bin): int
     {
-        if ($num > 15) {
-            throw new InvalidArgumentException("We need an integer between 0 and 15. We got " . var_export($num, true));
+        $dec = 0;
+        $pos = 0;
+        while ($bin) {
+            if ($pos === 0) {
+                $dec = ($bin & 1);
+            } else {
+                $dec += pow(2, $pos) * ($bin & 1);
+            }
+
+            $bin = $bin >> 1;
+            $pos++;
         }
 
-        return static::$charlist[$num];
-    }
-
-    /**
-     * Convert a decimal number to a hexadecimal character.
-     *
-     * @param string $char A hex chracter to be converted to value.
-     * @return int
-     * @throws InvalidArgumentException If the character is not valid.
-     */
-    private static function hexCharToValue(string $char) :int
-    {
-        $char = strtoupper($char);
-
-        if (strlen($char) !== 1) {
-            throw new InvalidArgumentException("We must only get one character. We got #" . strlen($char));
-        }
-
-        if (!in_array($char, str_split(static::$charlist), true)) {
-            throw new InvalidArgumentException("We need a valid hex character. We got " . var_export($char, true));
-        }
-
-        $pos = strpos(static::$charlist, $char);
-
-        return $pos;
-    }
-
-    /**
-     * Convert a string to hexadecimal value.
-     *
-     * @param string $string The string to convert.
-     * @return string the hexadecimal string.
-     */
-    public function fromString(string $string) :string
-    {
-        $bin = $this->stringToBin($string);
-        $hex = $this->binToHex($bin);
-
-        return $hex;
-    }
-
-    /**
-     * Convert a hexadecimal sequence to string.
-     *
-     * This might have nasty side effects if its not a string that's encoded in the hex.
-     *
-     * @param string $hex The hexadecimal sequence.
-     * @return string
-     */
-    public function toString(string $hex) :string
-    {
-        $bin = $this->hexToBinArray($hex);
-        $string = $this->binToString($bin);
-
-        return $string;
-    }
-
-    /**
-     * Convert the string to a binary representation.
-     *
-     * @param string $string The string to convert
-     * @return \GMP the binary string.
-     */
-    private function stringToBin(string $string) :\GMP
-    {
-        $bin = gmp_init(0, 10);
-        $strLength = strlen($string);
-
-        for ($pos = 0; $pos < $strLength; $pos++) {
-            $ord = gmp_init(ord($string[$pos]), 10);
-            // Reserve the max number of bits in memory and augment that.
-            $bin |= ($ord << (8 * ($strLength - $pos)));
-        }
-
-        return $bin;
+        return $dec;
     }
 
     /**
@@ -113,7 +41,7 @@ class Hex {
      * @param \GMP $bin the binary value as int
      * @return string The hexadecimal string
      */
-    private function binToHex(\GMP $bin) :string
+    private function binToHex(\GMP $bin): string
     {
         $hex = '';
 
@@ -130,22 +58,65 @@ class Hex {
 
         return $hex;
     }
-//
-//    /**
-//     * Convert a single ordinal value to a hexadecimal value.
-//     *
-//     * @param int $ord The ordinal value.
-//     * @return string The corresponding hexadecimal characters.
-//     */
-//    private static function ordinalToHex($ord) {
-//        $mod = $ord % 16;
-//
-//        if (($ord - $mod) === 0) {
-//            return self::toChar($ord);
-//        }
-//
-//        return self::ordinalToHex(($ord - $mod) / 16 ) . toChar($mod);
-//    }
+
+    /**
+     * Convert an array of binary values back to string.
+     *
+     * @param \GMP $bin
+     * @return string
+     */
+    private function binToString(\GMP $bin): string
+    {
+        $string = '';
+
+        $mask = 255; // Will give eight 1's as an eight bit mask.
+        while ($bin > 0) {
+            $byte = $bin & $mask;
+            $string .= chr($this->binToDec(gmp_intval($byte)));
+//            $string .= chr(gmp_intval($byte));
+            $bin = $bin >> 8;
+        }
+
+        return strrev($string);
+    }
+
+    /**
+     * Convert a string to hexadecimal value.
+     *
+     * @param string $string The string to convert.
+     * @return string the hexadecimal string.
+     */
+    public function fromString(string $string): string
+    {
+        $bin = $this->stringToBin($string);
+        $hex = $this->binToHex($bin);
+
+        return $hex;
+    }
+
+    /**
+     * Convert a decimal number to a hexadecimal character.
+     *
+     * @param string $char A hex chracter to be converted to value.
+     * @return int
+     * @throws InvalidArgumentException If the character is not valid.
+     */
+    private static function hexCharToValue(string $char): int
+    {
+        $char = strtolower($char);
+
+        if (strlen($char) !== 1) {
+            throw new InvalidArgumentException("We must only get one character. We got #" . strlen($char));
+        }
+
+        if (!in_array($char, str_split(static::$charlist), true)) {
+            throw new InvalidArgumentException("We need a valid hex character. We got " . var_export($char, true));
+        }
+
+        $pos = strpos(static::$charlist, $char);
+
+        return $pos;
+    }
 
     /**
      * Convert a hexadecimal sequence to binary.
@@ -153,7 +124,7 @@ class Hex {
      * @param string $hex The hexadecimal sequence.
      * @return \GMP
      */
-    private function hexToBinArray(string $hex) :\GMP
+    private function hexToBinArray(string $hex): \GMP
     {
         $hexLen = strlen($hex);
         $hex = strrev($hex);
@@ -169,45 +140,54 @@ class Hex {
     }
 
     /**
-     * Convert an array of binary values back to string.
+     * Convert the string to a binary representation.
      *
-     * @param \GMP $bin
-     * @return string
+     * @param string $string The string to convert
+     * @return \GMP the binary string.
      */
-    private function binToString(\GMP $bin) :string
+    private function stringToBin(string $string): \GMP
     {
-        $string = '';
+        $bin = gmp_init(0, 10);
+        $strLength = strlen($string);
 
-        $mask = 255; // Will give eight 1's as an eight bit mask.
-        while ($bin > 0) {
-            $byte = $bin & $mask;
-            $string .= chr($this->binToDec(gmp_intval($byte)));
-//            $string .= chr(gmp_intval($byte));
-            $bin = $bin >> 8;
+        for ($pos = 0; $pos < $strLength; $pos++) {
+            $ord = gmp_init(ord($string[$pos]), 10);
+            // Reserve the max number of bits in memory and augment that.
+            $bin |= ($ord << (8 * ($strLength - 1 - $pos)));
         }
 
-        return strrev($string);
+        return $bin;
     }
 
-    private function binToDec(int $bin) :int
+    /**
+     * Convert a decimal number to a hexadecimal character.
+     *
+     * @param int $num the number to convert to a character.
+     * @return string
+     * @throws InvalidArgumentException if the decimal number is 0 > $num > 15.
+     */
+    private static function toChar(int $num): string
     {
-        $dec = 0;
-        $pos = 0;
-        while ($bin) {
-            if ($pos === 0) {
-                $dec = ($bin & 1);
-            } else {
-                $dec += pow(2, $pos) * ($bin & 1);
-            }
-
-            $bin = $bin >> 1;
-            $pos++;
+        if ($num > 15) {
+            throw new InvalidArgumentException("We need an integer between 0 and 15. We got " . var_export($num, true));
         }
-// 0    0
-// 1    1
-// 2   10
-// 4  100
-// 8 1000
-        return $dec;
+
+        return static::$charlist[$num];
+    }
+
+    /**
+     * Convert a hexadecimal sequence to string.
+     *
+     * This might have nasty side effects if its not a string that's encoded in the hex.
+     *
+     * @param string $hex The hexadecimal sequence.
+     * @return string
+     */
+    public function toString(string $hex): string
+    {
+        $bin = $this->hexToBinArray($hex);
+        $string = $this->binToString($bin);
+
+        return $string;
     }
 }
